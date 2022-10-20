f61e9b26bdd426a16d1a7d4787abdc47bfc4da76
nicolasamedeiros <nicolas.amedeiros@gmail.com>
2022-10-19 21:15:46 -0300

    Questao_1

---
+++ Nicolas_Almeida_Medeiros.c
@@ -0,0 +1,16 @@
+//Prova Algoritimo
+
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+int main () {
+    printf("Disciplina: Algoritmo e Programacao\n");
+    printf("Professora: Joyce Siqueira\n");
+    printf("Aluno: Nicolas Almeida Medeiros\n");
+    printf("Matricula: uc221031171\n");  //terminar
+    printf("Curso: Engenharia de Software\n");
+    printf("Repositorio: https://github.com/nicolasamedeiros/Prova_algoritimo_ucb\n"); //terminar
+    printf("Software utilizado: VScode\n");
+    
+}
