f4b6dab2e676ac486e999b0db306a1bf25ee2bfd
nicolasamedeiros <nicolas.amedeiros@gmail.com>
2022-10-19 21:17:03 -0300

    Questao_2

--- Nicolas_Almeida_Medeiros.c
+++ Nicolas_Almeida_Medeiros.c
@@ -12,5 +12,24 @@ int main () {
     printf("Curso: Engenharia de Software\n");
     printf("Repositorio: https://github.com/nicolasamedeiros/Prova_algoritimo_ucb\n"); //terminar
     printf("Software utilizado: VScode\n");
+
+    char quest;
+    printf("Digite a letra da questao que deseja (obs: a, b,): ");
+    scanf("%c", &quest);
     
+    switch (quest)
+    {
+    case 'a':
+               
+        break;
+    
+    case 'b':
+        
+        break;
+    
+    
+    default:
+        printf("Opcao invalida");
+        break;
+    }
 }
